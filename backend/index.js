7937b63ab194e05149bb48dfb59c70f12ca38ff4
mattthewja <179329749+mattthewja@users.noreply.github.com>
2025-07-30 20:00:09 +0700

    /backend/index.js
    feat: add devMode to help with testing
    feat: add /getRoom and /getAllRooms routes
    fix: improve validating inputs in /deleteRoom route to handle edge cases

--- backend/index.js
+++ backend/index.js
@@ -64,20 +64,53 @@ app.post('/joinRoom', (req, res) => {
 
 // Delete room
 app.delete('/deleteRoom', (req, res) => {
-    const { roomId, username, owner } = req.body;
-    const room = rooms[roomId];
+    const { roomId, username } = req.body;
 
     // Validate input
-    if (!roomId || !username || !owner) {
+    if (!roomId || !username) {
+        return res.status(400).json({ error: 'roomId is required'});
+    }
+    const room = rooms[roomId];
+
+    if (!room) {
+        return res.status(404).json({ error: 'Room not found'});
+    }
+    const owner = rooms[roomId].owner;
+
+    if (owner != username) {
+        return res.status(403).json({ error: 'Only room owner can delete'});
+    }
+
+    delete(rooms[roomId]);
+    return res.status(200).json({ message: `Successfully deleted room ${room.name}`})
+})
+
+// Get room info
+app.get('/getRoom', (req, res) => {
+    const { roomId } = req.body;
+
+    // Validate input
+    if (!roomId) {
         return res.status(400).json({ error: 'roomId is required'});
     }
 
-    if (owner != username) {
-        return res.status(403).json({ error: 'only room owner can delete'});
+    const room = rooms[roomId];
+
+    if (!room) {
+        return res.status(404).json( {error: 'Room not found'})
     }
 
-    rooms[roomId].delete;
-    return res.status(200).json({ message: `Successfully deleted room ${room.name}`})
+    return res.json({ ...room, users: Array.from(room.users)});
+})
+
+// Get all roomId:Name //* WIP
+app.get('/getAllRooms', (req, res) => {
+    const roomSummaries = Object.entries(rooms).map(([roomId, room]) => ({
+        roomId,
+        name: room.name
+    }))
+
+    return res.json(roomSummaries);
 })
 
 //* Message routes
@@ -85,7 +118,6 @@ app.delete('/deleteRoom', (req, res) => {
 // Send message to a room
 app.post('/sendMessage', (req, res) => {
     const { roomId, message, username } = req.body;
-    const room = rooms[roomId]
 
     // Validate input
     if (!roomId || !message) {
@@ -95,10 +127,11 @@ app.post('/sendMessage', (req, res) => {
     if (!rooms[roomId]) {
         return res.status(404).json({ error: 'Room not found'});
     }
+    const room = rooms[roomId];
 
     // extreme case of user sending messages in a room without joining
     if (!room.users.has(username)) {
-        return res.status(403).json({ error: `You are not allowed to send messages in ${room.name}`});
+        return res.status(403).json({ error: `You are not allowed to send messages in ${roomId}:${room.name}`});
     }
 
     // add message to room
@@ -122,6 +155,10 @@ app.get('/getMessages', (req, res) => {
 
     res.status(200).json({ messages: rooms[roomId].messages })
 })
+
+
+
+
 
 //* Required for app to run. This opens the express app to be contactable.
 
